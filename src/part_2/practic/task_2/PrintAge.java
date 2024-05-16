d20903502cc19d16f6c0d8623a4a4228e54c4c4e
Liminiksi <luminiksi@ya.ru>
2024-05-16 19:17:40 +0300

    Практическая работа 2го модуля (раздел 2.12)

---
+++ src/part_2/practic/task_2/PrintAge.java
@@ -0,0 +1,46 @@
+package part_2.practic.task_2;
+
+public class PrintAge {
+    public static void main(String[] args) {
+        int vasyaAge = 29;
+        int katyaAge = 14;
+        int mishaAge = 36;
+
+        int min = -1;
+        int middle = -1;
+        int max = -1;
+
+        if (vasyaAge <= katyaAge && vasyaAge <= mishaAge) {
+            min = vasyaAge;
+            if (katyaAge >= mishaAge) {
+                max = katyaAge;
+                middle = mishaAge;
+            } else {
+                max = mishaAge;
+                middle = katyaAge;
+            }
+        } else if (katyaAge <= vasyaAge && katyaAge <= mishaAge) {
+            min = katyaAge;
+            if (vasyaAge >= mishaAge) {
+                max = vasyaAge;
+                middle = mishaAge;
+            } else {
+                max = mishaAge;
+                middle = vasyaAge;
+            }
+        } else {
+            min = mishaAge;
+            if (vasyaAge >= katyaAge) {
+                max = vasyaAge;
+                middle = katyaAge;
+            } else {
+                max = katyaAge;
+                middle = vasyaAge;
+            }
+        }
+
+        System.out.println("Minimal age: " + min);
+        System.out.println("Middle age: " + middle);
+        System.out.println("Maximal age: " + max);
+    }
+}
