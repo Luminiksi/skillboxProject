79cc8b3c3cc3544f430e25029549a4e1b57f839a
Liminiksi <luminiksi@ya.ru>
2024-05-16 16:36:19 +0300

    Практика из урока 2.3 (1 и 2 задание)

---
+++ src/part_2/lesson_4/conditions/Main.java
@@ -0,0 +1,19 @@
+package part_2.lesson_4.conditions;
+
+public class Main {
+    public static void main(String[] args) {
+        int maxWeight = 5000;
+        int minCost = 1000;
+
+        int weight = 3200;
+        int cost = 1750;
+
+        if (weight <= maxWeight && cost >= minCost) {
+            System.out.println("Доставка бесплатная");
+        }
+
+        if (weight > maxWeight || cost < minCost) {
+            System.out.println("Доставка платная");
+        }
+    }
+}
