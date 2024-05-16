a91c65f6d3999250e73097a752e21a6e3d7b8504
Liminiksi <luminiksi@ya.ru>
2024-05-16 16:42:16 +0300

    Из урока 1.8

---
+++ src/dateAndTime/Main.java
@@ -0,0 +1,10 @@
+package dateAndTime;
+
+import java.time.LocalDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Дата и время:");
+        System.out.println(LocalDateTime.now());
+    }
+}
