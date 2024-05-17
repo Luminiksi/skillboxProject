6da0432335c317976dd32ab672ce73ec6d519332
Liminiksi <luminiksi@ya.ru>
2024-05-17 19:06:19 +0300

    Из урока 3.2(v2)

--- src/cycle/Main.java
+++ src/cycle/Main.java
@@ -4,67 +4,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        Task 1
-        for (int i = 0; i < 5; i++) {
-            System.out.println("YES");
-        }
-        System.out.println();
-
-//        Task 2
-        int i;
-        for (i = 0; i < 5; i++) {
+//        Task 7
+        int i = 0;
+        for (; i < 100; i = i + 3) {
             System.out.println(i);
         }
-        System.out.println("After cycle");
-        System.out.println(i);
-        System.out.println();
-
-
-//        Task 3
-        for (int j = 15; j <= 40; j++) {
+//        Task 8 (бесконечный цикл)
+        /*for (int j = 0; ; j = j + 3) {
             System.out.println(j);
-            if(j == 16) {
-                System.out.println("Вы можете начать обучаться вождению");
-            }
-            if(j == 18) {
-                System.out.println("Вы можете получить водительские права");
-            }
-            if(j == 35) {
-                System.out.println("Вы можете баллотироваться на пост призедента");
-            }
+        }*/
+
+//        Task 9 (бесконечный цикл)
+        /*for (int j = 0; j < 100;) {
+            System.out.println(j);
+        }*/
+
+//        Task 9 (четные числа)
+        for (int j = 0; j < 100;) {
+            System.out.println(j);
+            j = j +2;
         }
-        System.out.println();
 
-//        Task 4
-        for (i = 50; i > 0; i--) {
-            System.out.println(i);
-        }
-        System.out.println();
-
-//        Task 5
-//        четные
-        for (i = 0; i <= 100; i = i + 2) {
-            System.out.println(i);
-        }
-        System.out.println();
-
-//        Task 5
-//        не четные
-        for (i = 1; i <= 100; i = i + 2) {
-            System.out.println(i);
-        }
-        System.out.println();
-
-
-//        Task 6
-        for (;;) {
-            System.out.println("Введите первое число:");
-            int value1 = new Scanner(System.in).nextInt();
-            System.out.println("Введите второе число:");
-            int value2 = new Scanner(System.in).nextInt();
-            int result = value1 * value2;
-            System.out.println("Произведение чисел равно: " + result);
-
-        }
     }
 }
