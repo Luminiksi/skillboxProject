d2b9936912e880abaa71f0e058e5a0e9078d7f96
Liminiksi <luminiksi@ya.ru>
2024-05-17 19:45:04 +0300

    Из урока 3.3

--- src/cycle/Main.java
+++ src/cycle/Main.java
@@ -4,26 +4,44 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        Task 7
-        int i = 0;
-        for (; i < 100; i = i + 3) {
-            System.out.println(i);
+/**
+ *  урок по while / do while
+ */
+        System.out.println("Практика по while / do while");
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println("Задание 1 - бесконечный цикл while");
+        while (true) {
+            System.out.println("hello");
         }
-//        Task 8 (бесконечный цикл)
-        /*for (int j = 0; ; j = j + 3) {
-            System.out.println(j);
-        }*/
+    }
 
-//        Task 9 (бесконечный цикл)
-        /*for (int j = 0; j < 100;) {
-            System.out.println(j);
-        }*/
+    public static void task2() {
+        System.out.println("Задание 2 - подсчет суммы");
+        int sum = 0;
+        int value = -1;
 
-//        Task 9 (четные числа)
-        for (int j = 0; j < 100;) {
-            System.out.println(j);
-            j = j +2;
+        while (value != 0) {
+            value = new Scanner(System.in).nextInt();
+            sum += value;
         }
 
+        System.out.println("Сумма введеных чисел равна " + sum);
+        System.out.println();
+    }
+
+    public static void task3() {
+        System.out.println("Задание 3 - подсчет суммы (for)");
+        int sum = 0;
+
+        for (int value = -1; value != 0;) {
+            value = new Scanner(System.in).nextInt();
+            sum += value;
+        }
+
+        System.out.println("Сумма введеных чисел равна " + sum);
+        System.out.println();
     }
 }
