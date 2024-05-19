e131dd2ce0eede18f60177bd2270d35ae057db8a
Liminiksi <luminiksi@ya.ru>
2024-05-19 15:37:04 +0300

    Практика из урока 4.5 (2е задание)

--- src/training/lesson_4_5/Methods.java
+++ src/training/lesson_4_5/Methods.java
@@ -3,14 +3,20 @@ package training.lesson_4_5;
 public class Methods {
     public static void main(String[] args) {
         Order order1 = new Order();
-        order1.addProduct(54);
-        order1.addProduct(600);
-        System.out.println("Итого по первому заказу: " + order1.getCount());
+        order1.addProduct(201, 40000);
+        order1.addProduct(317, 12000);
+        System.out.println("Информация по первому заказу:");
+        System.out.println("Количество :" + order1.getProductsCount());
+        System.out.println("Стоимость :" + order1.getTotalCount());
+        System.out.println();
 
         Order order2 = new Order();
-        order2.addProduct(436);
-        order2.addProduct(234);
-        order2.addProduct(34);
-        System.out.println("Итого по вторпому заказу: " + order2.getCount());
+        order2.addProduct(23, 5450);
+        order2.addProduct(45, 123300);
+        order2.addProduct(250, 34000);
+        System.out.println("Информация по второму заказу:");
+        System.out.println("Количество :" + order2.getProductsCount());
+        System.out.println("Стоимость :" + order2.getTotalCount());
+
     }
 }
--- src/training/lesson_4_5/Order.java
+++ src/training/lesson_4_5/Order.java
@@ -2,12 +2,18 @@ package training.lesson_4_5;
 
 public class Order {
     private int count = 0;
+    private int totalCount = 0;
 
-    public void addProduct(int productCount) {
+    public void addProduct(int productCount, int cost) {
         count = count + productCount;
+        totalCount += cost;
     }
 
-    public int getCount() {
+    public int getProductsCount() {
         return count;
+    }
+
+    public int getTotalCount() {
+        return totalCount;
     }
 }
