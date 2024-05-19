501343382f6a9f71952d9debed1d0e1b4bab9255
Liminiksi <luminiksi@ya.ru>
2024-05-19 16:25:35 +0300

    Практика из урока 4.6 (2е задание)

--- src/training/lesson_4_6/Main.java
+++ src/training/lesson_4_6/Main.java
@@ -2,12 +2,12 @@ package training.lesson_4_6;
 
 public class Main {
     public static void main(String[] args) {
-        User nastya = new User("Настя", 30);
-        User dima = new User("Дима", 31);
-        User lena = new User("Лена", 31);
-        printUserInfo(nastya);
-        printUserInfo(dima);
-        printUserInfo(lena);
+        Employee employee1 = new Employee("Сотрудник Один", "new@mail.com");
+        Employee employee2 = new Employee("Сотрудник Два", 4312);
+        Employee employee3 = new Employee("Сотрудник Три", "new2@mail.com", 2342);
+        employee1.printInfo();
+        employee2.printInfo();
+        employee3.printInfo();
     }
 
     public static void printUserInfo(User user) {
