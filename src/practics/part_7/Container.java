54ea97ab2d38838aeaf6ca37ace5cae5ef8af60f
Liminiksi <luminiksi@ya.ru>
2024-05-21 14:23:00 +0300

    Практическая работа 7го модуля (раздел 7.9 - исходник первого задания - из чего будем практиковаться)

---
+++ src/practics/part_7/Container.java
@@ -0,0 +1,13 @@
+package practics.part_7;
+
+public class Container {
+    private Integer count;
+
+    public void addCount(int value) {
+        count = count + value;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
