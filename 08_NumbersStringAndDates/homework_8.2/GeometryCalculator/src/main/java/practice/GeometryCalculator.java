946cd7420edd75ba8060da275d28ed86749fbf8f
Liminiksi <luminiksi@ya.ru>
2024-05-22 10:39:24 +0300

    Практика из урока 8.2 (2 задание - Решение)

--- 08_NumbersStringAndDates/homework_8.2/GeometryCalculator/src/main/java/practice/GeometryCalculator.java
+++ 08_NumbersStringAndDates/homework_8.2/GeometryCalculator/src/main/java/practice/GeometryCalculator.java
@@ -4,21 +4,34 @@ public class GeometryCalculator {
 
     // если значение radius меньше 0, метод должен вернуть -1
     public static double getCircleSquare(double radius) {
-        return 0.0;
+        if (radius < 0) {
+            return -1;
+        }
+        return Math.PI * Math.pow(radius, 2);
     }
 
     // если значение radius меньше 0, метод должен вернуть -1
     public static double getSphereVolume(double radius) {
-        return 0.0;
+        if (radius < 0) {
+            return -1;
+        }
+        return (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
     }
 
     public static boolean isTrianglePossible(double a, double b, double c) {
-        return false;
+        if (a >= (b + c) || b >= (a + c) || c >= (a + b)) {
+            return false;
+        }
+        return true;
     }
 
     // перед расчетом площади рекомендуется проверить возможен ли такой треугольник
     // методом isTrianglePossible, если невозможен вернуть -1.0
     public static double getTriangleSquare(double a, double b, double c) {
-        return 0.0;
+        if (!isTrianglePossible(a, b, c)) {
+            return -1;
+        }
+        double p = (a + b + c) / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
 }
