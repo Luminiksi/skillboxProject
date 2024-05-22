a997358e7af007d604cd4a7958b8f58b93aa6da0
Liminiksi <luminiksi@ya.ru>
2024-05-22 10:58:02 +0300

    Практика из урока 8.3 (1 задание - заготовка, что нам уже дано)

---
+++ src/training/lesson_8_3/UsageAnalytics.java
@@ -0,0 +1,13 @@
+package training.lesson_8_3;
+
+public class UsageAnalytics {
+    private int visitCount;
+
+    public UsageAnalytics() {
+        visitCount = 0;
+    }
+
+    public int countVisit() {
+        return visitCount++;
+    }
+}
