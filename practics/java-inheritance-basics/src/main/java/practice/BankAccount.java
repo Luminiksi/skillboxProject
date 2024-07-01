f5696890526f8b040dbb4a17fe0dc2c8581b26d5
Liminiksi <luminiksi@ya.ru>
2024-07-01 19:19:43 +0300

    13.8 Практическая работа

--- practics/java-inheritance-basics/src/main/java/practice/BankAccount.java
+++ practics/java-inheritance-basics/src/main/java/practice/BankAccount.java
@@ -1,20 +1,29 @@
 package practice;
 
 public class BankAccount {
+  protected double amoint;
 
   public double getAmount() {
     //TODO: реализуйте метод и удалите todo
     // верните значение количества денег не счету
-    return 0;
+    return amoint;
   }
 
   public void put(double amountToPut) {
     //TODO: реализуйте метод и удалите todo
     // метод зачисляет деньги на счет
+    if (amountToPut < 0) {
+      return;
+    }
+    amoint += amountToPut;
   }
 
   public void take(double amountToTake) {
     //TODO: реализуйте метод и удалите todo
     // метод списывает деньги со счета
+    if (amoint < amountToTake) {
+      return;
+    }
+    amoint -= amountToTake;
   }
 }
--- practics/java-inheritance-basics/src/main/java/practice/CardAccount.java
+++ practics/java-inheritance-basics/src/main/java/practice/CardAccount.java
@@ -3,4 +3,10 @@ package practice;
 public class CardAccount extends BankAccount {
     // не забывайте, обращаться к методам и конструкторам родителя
     // необходимо используя super, например, super.put(10D);
+
+    @Override
+    public void take(double amountToTake) {
+        amountToTake += amountToTake * 0.01;
+        super.take(amountToTake);
+    }
 }
