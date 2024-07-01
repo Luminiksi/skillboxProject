a08b67acaacea3381ecb429a0902dcfa526d1d78
Liminiksi <luminiksi@ya.ru>
2024-07-01 19:19:43 +0300

    13.8 Практическая работа

--- practics/java-inheritance-basics/src/main/java/practice/DepositAccount.java
+++ practics/java-inheritance-basics/src/main/java/practice/DepositAccount.java
@@ -1,5 +1,24 @@
 package practice;
 
-public class DepositAccount extends BankAccount {
+import java.time.Duration;
+import java.time.LocalDate;
 
+public class DepositAccount extends BankAccount {
+    private LocalDate lastIncome;
+
+    @Override
+    public void put(double amountToPut) {
+        super.put(amountToPut);
+        lastIncome = LocalDate.now();
+    }
+
+    @Override
+    public void take(double amountToTake) {
+        LocalDate now = LocalDate.now();
+        long days = Duration.between(lastIncome.atStartOfDay(), now.atStartOfDay()).toDays();
+        if (days < 31) {
+            return;
+        }
+        super.take(amountToTake);
+    }
 }
